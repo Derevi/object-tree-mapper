5e3bd4d8bdbb7c7d758637bf8a59d7d6787f2293
KevinKN-Auric <kknguyen@my.yorku.ca>
2018-08-01 16:39:40 -0400

    minor code clean up and updated todo lists

--- src/kkn/derevi/ClassGenerator.java
+++ src/kkn/derevi/ClassGenerator.java
@@ -13,7 +13,8 @@ public class ClassGenerator {
     public ClassGenerator() {
         //Todo read input and create classModel
         //todo allow for modification of any classModel as well as adding to and removing from list
-        //Todo create test class, check for null, and out of bounds exception,
+        //Todo create test class, check for null, and out of bounds exception
+        //Todo make serializable
 
 
 
